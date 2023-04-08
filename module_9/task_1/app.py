4b521e7abf8a695b0cd335acdee15aa07a998e6f
daler <halimovadalera@mail.ru>
2023-04-08 22:31:12 +0500

    docker final test

--- module_9/task_1/app.py
+++ module_9/task_1/app.py
@@ -1,15 +1,17 @@
+import os
 from flask import Flask
 
 
 HOST = '0.0.0.0'
 PORT = 5000
+SERVICE_NAME = os.environ.get('SERVICE_NAME', 'application')
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world() -> str:
-    return "<h1>Hello World</h1>"
+    return f"Hello World from {SERVICE_NAME}"
 
 
 if __name__ == '__main__':
